f78920d3385f321787c6bcfa6a7eaa863476d44a
MaterialFuture <konstantine@andrasmedia.org>
2021-05-16 18:09:27 -0500

    getting kjv to stay in cache

--- bible.lisp
+++ bible.lisp
@@ -1,36 +1,47 @@
 ;;; Bible script for reading and searching for bible passages
 
-(ql:quickload '("yason" "dexador"))
+(ql:quickload '("yason"
+                "trivial-download"))
 
 ;;; Parameters
-
 ;; (defparameter *bible* "King James Version")
 ;; (defparameter *bible-abbr* 'kjv)
 ;; (defparameter *book-name* 'Genesis)
 ;; (defparameter *book-abbr* 'gen)
 (defparameter *book-num* 1)
+(defparameter *book-abrv* 'gen)
 (defparameter *chapter-num* 001)
 (defparameter *verse-num* 001)
-(defparameter *verseid* 1001001 )
+
+(defparameter *bible-data* nil)
+(defparameter *current-ch-data* nil)
+
+(defvar data-url "https://raw.githubusercontent.com/scrollmapper/bible_databases/master/json/t_kjv.json")
+(defvar json-data-cache-loc "$HOME/.cache/")
 
 ;;; Main Functions
- 
-(defun get-file (file)
-  (reverse
-   (yason:parse
-    (dex:get file :want-stream t)
-    :object-as :alist
-    :json-arrays-as-vectors t)))
+
+(defun get-bible-data () ; "https://raw.githubusercontent.com/thiagobodruk/bible/master/json/en_kjv.json"
+  ;; if download success then do yason parse
+  (trivial-download:download data-url
+                             json-data-cache-loc)
+  (setq *bible-data*
+    (reverse
+     (yason:parse
+       json-data-cache-loc
+      ;; (dex:get "https://raw.githubusercontent.com/scrollmapper/bible_databases/master/json/t_kjv.json" :want-stream t) ; online get method
+; TODO find way to store file in ~/.cache for linux
+; Have both JSON and list formats available in cache
+       :object-as :alist
+       :json-arrays-as-vectors t))))
 
 ;; Checks
 (defun get-current-verseid ()
   (princ *verseid*))
 
-(defun check-verseid ())
-  ;; compare verseid from param or from current and check if valid before going to next one 
-
 ;;; Navigation
-;;; 
+;; TODO Since Yason converts json to lists then try to navigate the list in a way that makes sense
+;; If I decide to provide a buffer or some sort of bookmark system then the (setq x (+ x 1)) system is nice to keep track of things
 ;; Verses
 (defun next-verse ()
   ;; (setq (*verseid* (+ *verseid* 1)))
@@ -60,5 +71,7 @@
 
 
 (defun print-all-book-names ()
+  "Prints all book names so users can quickly navigate"
+  ;TODO programatically generate this from the list
   (format t "Print all the book names plus numbers, (ie. 1.  Genesis...)"))
 
