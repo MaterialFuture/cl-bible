a7ebf65a2dd9bd360bf68fef4e584e0329659cf2
materialfuture <konstantinev@protonmail.com>
2021-06-16 21:57:51 -0500

    added todos for verses and updated books header

--- src/verses.lisp
+++ src/verses.lisp
@@ -16,3 +16,18 @@
     (loop :for line := (read-line *standard-input* nil)
           :while line
           :collect (split-string-with-delimiter line :delimiter #\Tab))))
+
+;; TODO - Finish main functions
+
+(defun search-books (book)
+  "Search the books in the bible based on PRINT-ALL-BOOKS"
+  (format T "~d" book))
+
+(defun search-verse (book verse)
+  "Search bible for verse and return
+TODO: parse verse for colons"
+  (format T "~s, ~s" book verse))
+
+(defun search-bible (string)
+  "Search all of bible for string and return all matching cases as list of strings"
+  (format T "~s" string))
